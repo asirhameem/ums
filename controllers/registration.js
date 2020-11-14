0b88c9d04f7c5da293ea8ddcf4f3cc356aefeef2
Asir Hameem <56108077+asirhameem@users.noreply.github.com>
2020-11-15 02:38:49 +0600

    Registration Added

---
+++ controllers/registration.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const db = require.main.require('./models/db');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('Register');
+})
+
+router.post('/', (req, res) => {
+
+    var sql = "select * from user where username='" + req.body.username + "' and password='" + req.body.password + "'";
+    db.getResults(sql, function(results) {
+        if (results.length > 0) {
+            res.cookie('uname', req.body.username);
+            res.redirect('/home');
+        } else {
+            res.redirect('/login');
+        }
+    });
+})
+
+module.exports = router;
